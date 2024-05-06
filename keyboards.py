295f2ca67247e1b944f81fa0a064e7e40be6e954
matvey123085347 <59483460+matvey123085347@users.noreply.github.com>
2024-05-06 21:46:36 +0300

    Update keyboards.py

--- keyboards.py
+++ keyboards.py
@@ -1,4 +1,4 @@
 from aiogram.types import InlineKeyboardMarkup, KeyboardButton, WebAppInfo
 
 def web_app():
-	return InlineKeyboardMarkup().add(KeyboardButton('open', web_app=WebAppInfo(url='...')))
+	return InlineKeyboardMarkup().add(KeyboardButton('open', web_app=WebAppInfo(url='https://github.com/matvey123085347/TelegramWebApp.git')))
